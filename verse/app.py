20f9276a3958aa2889e3acf074a9ce8e25e72889
ElecTrOoD <32058883+ElecTrOoD@users.noreply.github.com>
2022-01-24 20:55:27 +0300

    minor fix

--- verse/app.py
+++ verse/app.py
@@ -43,14 +43,18 @@ class Application:
     
     @staticmethod
     def get_qs_data(environ):
-        query_string = parse_qs(environ['QUERY_STRING'])
-        parsed_data = dict(map(lambda x: (x[0], x[1][0]), query_string.items()))
-        return parsed_data
+        if environ['QUERY_STRING']:
+            query_string = parse_qs(environ['QUERY_STRING'])
+            parsed_data = dict(map(lambda x: (x[0], x[1][0]), query_string.items()))
+            return parsed_data
+        return {}
 
     @staticmethod
     def get_post_data(environ):
         content_length = int(environ.get('CONTENT_LENGTH', 0))
-        content_data = environ['wsgi.input'].read(content_length)
-        parsed_data = dict(map(lambda x: (x[0].decode(
-            'utf-8'), x[1][0].decode('utf-8')), parse_qs(content_data).items()))
-        return parsed_data
+        if content_length:
+            content_data = environ['wsgi.input'].read(content_length)
+            parsed_data = dict(map(lambda x: (x[0].decode(
+                'utf-8'), x[1][0].decode('utf-8')), parse_qs(content_data).items()))
+            return parsed_data
+        return {}
