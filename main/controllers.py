c981e143926baecc223adfecebe5a7628be292a1
ElecTrOoD <32058883+ElecTrOoD@users.noreply.github.com>
2022-01-27 07:36:40 +0300

    changing arguments

--- main/controllers.py
+++ main/controllers.py
@@ -1,18 +1,18 @@
 from verse import render
 
 
-def index_page(request, template_dir):
+def index_page(request, template_dir='templates'):
     context = {'title': 'Main'}
-    return '200 OK', render(request, f'{template_dir}main\\index.html', context)
+    return '200 OK', render(request, 'main/index.html', context)
 
 
-def about_page(request, template_dir):
+def about_page(request, template_dir='templates'):
     context = {'title': 'About'}
-    return '200 OK', render(request, f'{template_dir}main\\about.html', context)
+    return '200 OK', render(request, 'main/about.html', context)
 
-def contacts(request, template_dir):
+def contacts(request, template_dir='templates'):
     context = {'title': 'Contacts'}
     if request['method'] == 'POST':
         context['message'] = request['form']
-        return '200 OK', render(request, f'{template_dir}main\\user_message.html', context)
-    return '200 OK', render(request, f'{template_dir}main\\contacts.html', context)
+        return '200 OK', render(request, 'main/user_message.html', context)
+    return '200 OK', render(request, 'main/contacts.html', context)
