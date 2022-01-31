af3b0de25b74525b5c180a622c5f76d6cbb0abee
ElecTrOoD <32058883+ElecTrOoD@users.noreply.github.com>
2022-01-31 16:03:10 +0300

    removed unused code

--- main/controllers.py
+++ main/controllers.py
@@ -16,10 +16,3 @@ class ContactsPage(FormController):
     template_name = 'main/contacts.html'
     success_template = 'main/user_message.html'
     form_fields = ['title', 'text', 'email']
-
-# def contacts(request, template_dir='templates'):
-#     context = {'title': 'Contacts'}
-#     if request['method'] == 'POST':
-#         context['message'] = request['form']
-#         return '200 OK', render(request, 'main/user_message.html', context)
-#     return '200 OK', render(request, 'main/contacts.html', context)
