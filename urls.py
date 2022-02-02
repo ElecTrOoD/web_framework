29cdb4526e59df0e31e102cc8de137431a865399
ElecTrOoD <32058883+ElecTrOoD@users.noreply.github.com>
2022-02-02 22:16:17 +0300

    added new urls

--- urls.py
+++ urls.py
@@ -1,12 +1,19 @@
 # example '<path>': {'controller': <controller func>, 'allowed_methods': (<HTTP method>)}
 #         '/': {'controller': index, 'allowed_methods': ('GET', )},
-
+from courses.controllers import CoursesListPage, CoursesCreatePage, CoursePage, CourseDelete, \
+    CategoriesListPage, CategoryCreatePage, CategoryDelete, CoursesCopyPage
 from main.controllers import IndexPage, AboutPage, ContactsPage
 
 urlpatterns = {
     '/': {'controller': IndexPage, 'allowed_methods': ('GET',)},
     '/about/': {'controller': AboutPage, 'allowed_methods': ('GET', 'POST')},
-    '/contacts/': {'controller': ContactsPage, 'allowed_methods': ('GET', 'POST')}
+    '/contacts/': {'controller': ContactsPage, 'allowed_methods': ('GET', 'POST')},
+    '/courses/': {'controller': CoursesListPage, 'allowed_methods': ('GET',)},
+    '/course/': {'controller': CoursePage, 'allowed_methods': ('GET',)},
+    '/courses/create/': {'controller': CoursesCreatePage, 'allowed_methods': ('GET', 'POST')},
+    '/courses/copy/': {'controller': CoursesCopyPage, 'allowed_methods': ('POST',)},
+    '/courses/delete/': {'controller': CourseDelete, 'allowed_methods': ('GET',)},
+    '/categories/': {'controller': CategoriesListPage, 'allowed_methods': ('GET',)},
+    '/categories/create/': {'controller': CategoryCreatePage, 'allowed_methods': ('GET', 'POST')},
+    '/categories/delete/': {'controller': CategoryDelete, 'allowed_methods': ('GET',)},
 }
-
-
